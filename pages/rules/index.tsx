255004a5d3ef47a778a7af2ba8bdbadd5b185892
Sebastian Ahlman <hawaiilive@outlook.com>
2022-11-10 14:12:11 +0100

    import link

--- pages/rules/index.tsx
+++ pages/rules/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/Link";
 import Header from "../../Components/Header";
 import Info from "../../Components/Info";
 import Rule from "../../Components/Rule";
