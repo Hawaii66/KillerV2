950dbabc2e67f2a8bdd9e3d0631f44290a87d9a5
Sebastian Ahlman <hawaiilive@outlook.com>
2022-11-12 19:12:18 +0100

    Stats

--- pages/stats/index.tsx
+++ pages/stats/index.tsx
@@ -10,8 +10,10 @@ import {
   Title,
 } from "chart.js";
 import { Bar, Pie } from "react-chartjs-2";
-import { type } from "os";
 import useWindowSize from "../../Hooks/WindowSize";
+import { connect, dbs } from "../../utils/DBConnection";
+import { KillerUser } from "../../Interfaces/Interfaces";
+import styles from "./stats.module.css";
 
 interface GroupStats {
   groupName: string;
@@ -81,118 +83,9 @@ const optionsKills = {
   },
 };
 
-function Stats() {
+function Stats({ groups }: { groups: GroupStats[] }) {
   const size = useWindowSize();
-  const [groups, setGroups] = useState<GroupStats[]>([
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-    {
-      alive: 5,
-      groupName: "Na21B",
-      kills: 25,
-      total: 25,
-    },
-    {
-      alive: 15,
-      groupName: "Na21A",
-      kills: 10,
-      total: 20,
-    },
-  ]);
+  const [selectedGroup, setSelected] = useState<GroupStats>(groups[0]);
 
   return (
     <div style={{ width: size.width < 800 ? "95vw" : 500 }}>
@@ -232,41 +125,92 @@ function Stats() {
           ],
         }}
       />
-      {groups.map((group, index) => {
-        return (
-          <Pie
-            key={index}
-            options={{
-              plugins: {
-                title: {
-                  text: `${group.groupName}`,
-                  display: true,
-                },
+      <div className={styles.wrapper}>
+        <select>
+          {groups.map((group, index) => {
+            return (
+              <option key={group.groupName} value={group.groupName}>
+                {group.groupName}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {selectedGroup.groupName !== "" && (
+        <Pie
+          options={{
+            color: "rgba(0,0,0,1)",
+          }}
+          data={{
+            labels: [
+              `Levande: ${selectedGroup.alive}`,
+              `Döda: ${selectedGroup.total - selectedGroup.alive}`,
+            ],
+            datasets: [
+              {
+                label: "Antal levande",
+                data: [
+                  selectedGroup.alive,
+                  selectedGroup.total - selectedGroup.alive,
+                ],
+                backgroundColor: [
+                  "rgba(130, 205, 71, 0.3)",
+                  "rgba(225, 77, 42, 0.3)",
+                ],
+                borderColor: [
+                  "rgba(130, 205, 71, 0.5)",
+                  "rgba(225, 77, 42, 0.5)",
+                ],
               },
-              color: "rgba(0,0,0,1)",
-            }}
-            data={{
-              labels: ["Levande", "Döda"],
-              datasets: [
-                {
-                  label: "Antal levande",
-                  data: [group.alive, group.total - group.alive],
-                  backgroundColor: [
-                    "rgba(130, 205, 71, 0.3)",
-                    "rgba(225, 77, 42, 0.3)",
-                  ],
-                  borderColor: [
-                    "rgba(130, 205, 71, 0.5)",
-                    "rgba(225, 77, 42, 0.5)",
-                  ],
-                },
-              ],
-            }}
-          />
-        );
-      })}
+            ],
+          }}
+        />
+      )}
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  await connect();
+  const users: KillerUser[] = await dbs.users.find();
+
+  var groups: { [key: string]: GroupStats } = {};
+  users.forEach((user) => {
+    if (groups[user.group] === undefined) {
+      groups[user.group] = {
+        alive: 0,
+        groupName: user.group,
+        kills: 0,
+        total: 0,
+      };
+    }
+    groups[user.group].alive += user.alive ? 1 : 0;
+    groups[user.group].kills += user.kills;
+    groups[user.group].total += 1;
+    groups[user.group].groupName = user.group;
+  });
+
+  var finals: GroupStats[] = [];
+  for (const [key, value] of Object.entries(groups)) {
+    finals.push(value);
+  }
+
+  finals.sort((a, b) => {
+    return a.groupName.localeCompare(b.groupName);
+  });
+
+  for (var i = 0; i < finals.length; i++) {
+    if (finals[i].groupName === "Lärare") {
+      finals.push({ ...finals[i] });
+      finals.splice(i, 1);
+    }
+  }
+
+  return {
+    props: {
+      groups: finals,
+    },
+  };
 }
 
 export default Stats;
